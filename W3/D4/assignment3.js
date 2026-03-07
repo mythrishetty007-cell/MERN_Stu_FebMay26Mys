a8143c2a3ffe17c62cf7f668122e3e54fbcb9dfe
mythrishetty007-cell <mythrishetty007@gmail.com>
2026-03-07 21:03:40 +0530

    Support Ticket Queue Processor

---
+++ W3/D4/assignment3.js
@@ -0,0 +1,29 @@
+// Assignment 3
+// 1. Initial queue with at least 5 tickets
+let tickets = [
+    { id: "T101", priority: "HIGH", resolved: false },
+    { id: "T102", priority: "MEDIUM", resolved: true },
+    { id: "T103", priority: "LOW", resolved: false },
+    { id: "T104", priority: "HIGH", resolved: false },
+    { id: "T105", priority: "MEDIUM", resolved: true }
+  ];
+  
+  tickets.unshift({ id: "T100", priority: "HIGH", resolved: false });
+  
+  tickets.push(
+    { id: "T106", priority: "LOW", resolved: false },
+    { id: "T107", priority: "MEDIUM", resolved: true }
+  );
+  
+  let currentTicket = tickets.shift();
+  
+  let droppedTicket = tickets.pop();
+  
+  let pending = tickets.filter(ticket => ticket.resolved === false);
+  
+  let pendingIds = pending.map(ticket => ticket.id);
+  
+  console.log("Current Ticket:", currentTicket);
+  console.log("Dropped Ticket:", droppedTicket);
+  console.log("Pending Tickets:", pending);
+  console.log("Pending Ticket IDs:", pendingIds);
