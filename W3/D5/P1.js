9b648eacf4c6abccad24d4af2248135bb4afde04
mythrishetty007-cell <mythrishetty007@gmail.com>
2026-03-07 19:56:08 +0530

    Basic of Click Events

---
+++ W3/D5/P1.js
@@ -0,0 +1,11 @@
+// Basics of Click events
+
+const button = document.getElementById("clickBtn");
+
+button.addEventListener("click",function() {
+    console.log("Button is clicked");
+});
+
+button.addEventListener("click",function() {
+    console.log("Second event listener: Button is clicked");
+});
