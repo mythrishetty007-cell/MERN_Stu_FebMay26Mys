82924c08a71bb145aac8d2d1088d9da35bd8245f
mythrishetty007-cell <mythrishetty007@gmail.com>
2026-03-17 23:35:31 +0530

    FAQ Builder with Dynamic Sections

--- Assignment/JavaScript Form Validation/assign2.js
+++ Assignment/JavaScript Form Validation/assign2.js
@@ -1,71 +1,47 @@
-const addBtn = document.getElementById("addBtn");
-const deleteBtn = document.getElementById("deleteBtn");
 const ans = document.getElementById("ans");
 const question = document.getElementById("question"); 
-let counter = 1;
 const answer = document.getElementById("answer");
-const container = document.getElementById("container");
-
-question.addEventListener("input",function(){
-const quest = question.value;
-if(!quest.includes('?')){
-    ans.textContent = "Question is required.";
-    ans.style.color = "red";
-    question.focus();
-    return;
-}
-if(quest.length < 5){
-    ans.textContent = "question required, minimum 5 characters";
-    ans.style.color = "red";
-    question.focus();
-    return;
-}
-else{
-        ans.textContent = "Entered correct!";
-        ans.style.color = "green";
-    }
-});
-
-answer.addEventListener("input",function(){
-    const answers = answer.value;
-    if(!answers.includes('.')){
-        ans.textContent = "Answer is required.";
-        ans.style.color = "red";
-        answer.focus();
-        return;
-    }
-    if(answers.length < 15){
-        ans.textContent = "answer required, minimum 15 characters";
-        ans.style.color = "red";
-        answer.focus();
-        return;
-    }
-    else{
-        ans.textContent = "Entered correct!";
-        ans.style.color = "green";
-    }
-});
-
-addBtn.addEventListener("click",function(){
-    const add = document.createElement("p");
-    add.classList.add("faq-item");
-    add.textContent = "FAQ manager " + counter++;
-    container.append(add);
-});
-
-deleteBtn.addEventListener("click",function(){
-    const del = document.getElementsByClassName("faq-Item");
-    if(del){
-        ans.textContent = "No FAQs available";
-        del.remove();
-        counter--;
-    }
-    else{
-        ans.textContent = "Available";
-    }
-});
-
+const faqcontainer = document.getElementById("faqcontainer");
 
 document.getElementById("addBtn").addEventListener("click",function(){
-container.insertAdjacentHTML("beforeend","<p>Dynamically inserted</p>");
+    if(!question.value){
+        ans.textContent = "Question is required";
+        question.focus();
+        return;
+    }
+    if(question.value.length<5){
+        ans.textContent = "Question must be atleast or minimum 5 characters";
+        question.focus();
+        return;
+    }
+    if(answer.value.length<15){
+        ans.textContent = "Answer must be atleast or minimum 15 characters";
+        answer.focus();
+        return;
+    }
+    ans.textContent = "";
+    const h2 = document.createElement("h2");
+    const p = document.createElement("p");
+
+    h2.textContent = question.value;
+    p.textContent = answer.value;
+
+    faqcontainer.appendChild(h2);
+    faqcontainer.appendChild(p);
+
+    question.value = "";
+    answer.value = "";
+});
+
+document.getElementById("deleteBtn").addEventListener("click",function(){
+    if(faqcontainer.lastElementChild){
+        faqcontainer.removeChild(faqcontainer.lastElementChild);
+    }
+    else if(!faqcontainer===-1){
+        ans.textContent = "FAQ avaliable";
+        return;
+    }
+    else{
+        ans.textContent = "No FAQ avaliable";
+    }
 });
