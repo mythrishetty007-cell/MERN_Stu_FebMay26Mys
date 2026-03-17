93884e08625994eec3ecfa5ca85cf899c3f4c2e6
mythrishetty007-cell <mythrishetty007@gmail.com>
2026-03-17 13:31:35 +0530

    Writing custom callback functions

---
+++ W5/D2/P4.js
@@ -0,0 +1,34 @@
+//Writing custom callback functions
+function processStudent(name,score,callback,monkey){
+    console.log("Student name: ",name);
+    console.log("Score: ",score);
+
+    callback(name,score);
+    monkey(name,score);
+}
+
+function showResult(name,score){
+    if(score>=70){
+        console.log(name+ " has passed.");
+    }
+    else{
+        console.log(name+ " has failed")
+    }
+}
+
+function showGrade(name,score){
+    if(score >= 85){
+        console.log("Grade: A+");
+    }
+    else if(score >= 75){
+        console.log("Grade: A");
+    }
+    else if(score >= 70){
+        console.log("Grade: B");
+    }
+    else{
+        console.log("Grade: Fail");
+    }
+}
+// processStudent("Baji Rao",87,showResult);
+processStudent("Baji Rao",87,showResult,showGrade);
