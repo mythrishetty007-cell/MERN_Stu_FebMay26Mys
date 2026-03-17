bbdb5bb7268f98e29d6ce1c6ada4d732c323e071
mythrishetty007-cell <mythrishetty007@gmail.com>
2026-03-17 16:21:49 +0530

    Chaining promises with centralized error handling

---
+++ W5/D2/P12.js
@@ -0,0 +1,32 @@
+//Chaining promises with centralized error handling
+function validateLogin(){
+  return new Promise(function(resolve){
+    setTimeout(function(){
+        resolve("Login Validated");
+    });
+  });  
+} 
+function fetchAccountData(){
+    return new Promise(function(resolve,reject){
+        setTimeout(function(){
+            const accountFound = true;
+            if(accountFound){
+                resolve("Account data loaded.");
+            }
+            else{
+                reject("Account data could not be found")
+            }
+        })
+    });
+}
+validateLogin()
+.then(function(message){
+    console.log(message);
+    return fetchAccountData();
+})
+.then(function(accountMessage){
+    console.log(accountMessage);
+})
+.catch(function(error){
+    console.log("Chain error: ",error);
+});
