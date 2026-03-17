f7b6ed0e45135f5fc342f02c73d9949191f29151
mythrishetty007-cell <mythrishetty007@gmail.com>
2026-03-17 22:35:17 +0530

    Employee payroll records

--- Assignment/JavaScript Arrays/assignment1.js
+++ Assignment/JavaScript Arrays/assignment1.js
@@ -1,7 +1,7 @@
 // Assignment 1
 // Employee payroll records
 let employees = [
-    { name: "Ganesh", basePay: 70000, bonus: 4000, taxRate: 0 },
+    { name: "Ganesha", basePay: 70000, bonus: 4000, taxRate: 0 },
     { name: "Shiva", basePay: 47000, bonus: 1300, taxRate: 0.01 },
     { name: "Meghana", basePay: -500, bonus: 1000, taxRate: 0.7 }, 
     { name: "Arpitha", basePay: 60000, bonus: 5000, taxRate: 0.14 }
