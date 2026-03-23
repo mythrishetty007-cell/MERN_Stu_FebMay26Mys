550e9df32751d7c5d7f041d849097744d78b7ff3
mythrishetty007-cell <mythrishetty007@gmail.com>
2026-03-23 12:32:21 +0530

    commonJS export and import

---
+++ W6/D1/P2.js
@@ -0,0 +1,15 @@
+// commonJS export and import
+
+function calculateTax(amount){
+    return amount*0.18;
+}
+
+function applyDiscount(amount,percent){
+    return amount - amount * (percent/100);
+}
+
+function formatCurrency(amount){
+    return "INR "+amount.toFixed(2); //toFixed is used for floating point 
+}
+// module.exports makes these functions available to require()
+module.exports = {calculateTax,applyDiscount,formatCurrency};
