8f2445924839182a242a85078dd1929d5de62e40
mythrishetty007-cell <mythrishetty007@gmail.com>
2026-03-30 11:33:41 +0530

    Handling different http methods in express

---
+++ W7/D1/P3.js
@@ -0,0 +1,20 @@
+//handling different http methods in express
+
+const express=require("express");
+
+const app=express();
+
+app.get("/users",function(req,res){
+    res.send("returning all users");
+});
+
+//To create
+app.post("/users",function(req,res){
+    // res.status() sets the HTTP status code 
+    // before sending the response body
+    res.status(201).send("User created.");    
+});
+
+app.listen(4000,function(){
+    console.log("Express server running at http://localhost:4000");
+});
