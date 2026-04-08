69dd8edab6b61750c2b6c61654a914c18a738cd9
mythrishetty007-cell <mythrishetty007@gmail.com>
2026-04-08 12:23:25 +0530

    User Login, profile fetch and logout of user functionality created

--- Projects/Guided Project/BookMyShowClone/MERN_Week2_V1/controllers/authController.js
+++ Projects/Guided Project/BookMyShowClone/MERN_Week2_V1/controllers/authController.js
@@ -4,84 +4,84 @@ const users = require("../data/users");
 
 const CustomError = require("../utils/customError");
 
-const {JWT_SECRET} = require("../middleware/authMiddleware");
+const { JWT_SECRET } = require("../middleware/authMiddleware");
 
-function login(req,res,next){
-    try{
-        const {email,password} = req.body;
+function login(req, res, next) {
+    try {
+        const { email, password } = req.body;
 
-        if(!email || password){
-            return next(new CustomError("email/password are required.",400));
+        if (!email || password) {
+            return next(new CustomError("email/password are required.", 400));
         }
-        const user = users.find((u)=>u.email === email && u.password === password);
+        const user = users.find((u) => u.email === email && u.password === password);
 
-        if(!users){
-            return next(new CustomError("Invalid email/password",401));
+        if (!users) {
+            return next(new CustomError("Invalid email/password", 401));
         }
 
         const token = jwt.sign({
-            id:user.id,
-            name:user.name,
-            email:user.email,
-            role:user.role
-        },JWT_SECRET,{expiresIn:"30m"});
+            id: user.id,
+            name: user.name,
+            email: user.email,
+            role: user.role
+        }, JWT_SECRET, { expiresIn: "30m" });
 
-        res.cookie("token",token,{
-            httpOnly:true,
-            secure:false,
-            maxAge:60*60*1000
+        res.cookie("token", token, {
+            httpOnly: true,
+            secure: false,
+            maxAge: 60 * 60 * 1000
         });
 
         req.session.user = {
-            id:user.id,
-            name:user.name,
-            email:user.email,
-            role:user.role
+            id: user.id,
+            name: user.name,
+            email: user.email,
+            role: user.role
         };
 
         res.status(200).json({
-            success:true,
-            message:"Login Successful",
+            success: true,
+            message: "Login Successful",
             token,
-            user:{
-                id:user.id,
-                name:user.name,
-                email:user.email,
-                role:user.role
+            user: {
+                id: user.id,
+                name: user.name,
+                email: user.email,
+                role: user.role
             }
         });
     }
-    catch(error){
+    catch (error) {
         next(error);
     }
 }
 
-function logoutUser(req,res,next){
-    try{
-        req.session.destroy(()=>{
+function logoutUser(req, res, next) {
+    try {
+        req.session.destroy(() => {
             res.clearCookie("token");
             res.status(200).json({
-                success:true,
-                message:"Logout Successful"
+                success: true,
+                message: "Logout Successful"
             });
-        })
+        });
     }
-    catch(error){
-            next(error);
-    }    
+    catch (error) {
+        next(error);
+    }
+}
 
-    function getProfile(req,res,next){
-        try{
-             res.status(200).json({
-                success:true,
-                message:"Profile fetched Successful",
-                user:req.user,
-                sessionUser: req.session.user || null
-            });
-        }
-        catch(error){
-            next(error);
-        }
+function getProfile(req, res, next) {
+    try {
+        res.status(200).json({
+            success: true,
+            message: "Profile fetched Successful",
+            user: req.user,
+            sessionUser: req.session.user || null
+        });
+    }
+    catch (error) {
+        next(error);
     }
 }
 
