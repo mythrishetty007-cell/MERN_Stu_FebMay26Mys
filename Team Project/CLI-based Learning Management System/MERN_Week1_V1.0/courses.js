8121f6651dfdd6688ec43eae7b2b40c938e8bbdc
mythrishetty007-cell <mythrishetty007@gmail.com>
2026-04-14 16:46:28 +0530

    LMS TeamProject

--- Team Project/CLI-based Learning Management System/MERN_Week1_V1.0/courses.js
+++ Team Project/CLI-based Learning Management System/MERN_Week1_V1.0/courses.js
@@ -2,29 +2,41 @@ const courses = [
     {
         id:1,
         title:"Frontend Developer",
+        instructor:"Rakesh",
         description:"Client based development",
+        total_lessons:3,
         lessons:[
             {name:"HTML"},
             {name:"CSS"},
             {name:"JavaScript"}
         ],
-        limit:50
+        level:"Easy",
+        category:"Web Development",
+        limit:50,
+        enroll_status:20
     },
     {
         id:2,
         title:"Backend Developer",
+        instructor:"Rakesh",
         description:"Server based development",
+        total_lessons:3,
         lessons:[
             {name:"Python"},
             {name:"MongoDB"},
             {name:"Java"}
         ],
-        limit:100
+        level:"Modern",
+        category:"Database",
+        limit:100,
+        enroll_status:50
     },
     {
         id:3,
         title:"Full Stack Developer",
+        instructor:"Rakesh",
         description:"Client and Server based development",
+        total_lessons:5,
         lessons:[
             {name:"HTML"},
             {name:"CSS"},
@@ -32,7 +44,26 @@ const courses = [
             {name:"NodeJS"},
             {name:"ExpressJS"}
         ],
-        limit:150
+        level:"Modern",
+        category:"Programming",
+        limit:150,
+        enroll_status:75
+    },
+    {
+        id:3,
+        title:"Verbal Ability",
+        instructor:"Viganesh",
+        description:"Communication based Skills",
+        total_lessons:5,
+        lessons:[
+            {name:"Grammer"},
+            {name:"Tenses"},
+            {name:"Formal Writing"}
+        ],
+        level:"Modern",
+        category:"Soft Skills",
+        limit:100,
+        enroll_status:40
     }
 ];
 
