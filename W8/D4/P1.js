8c13ec8f8a503177984683feb23a7ec2f3098157
mythrishetty007-cell <mythrishetty007@gmail.com>
2026-04-15 12:47:45 +0530

    Date fundamentals

---
+++ W8/D4/P1.js
@@ -0,0 +1,32 @@
+//Date fundamentals
+const mongoose = require("mongoose");
+const { log } = require("node:console");
+
+async function main(){
+    try{
+        await mongoose.connect('mongodb://localhost:27017/embrel');
+        console.log("Connected to MongoDB");
+        const schema = new mongoose.Schema({
+            name:String,
+            createdAt:{
+                type:Date,
+                default:Date.now
+            }
+        });
+        const Model = mongoose.model('DateFund',schema);
+        await Model.deleteMany();
+        
+        const doc = await Model.create({
+            name:"Test"
+        });
+        console.log("Document:",doc);
+    }
+    catch(err){
+        console.error("Error:",err.message);   
+    }
+    finally{
+        await mongoose.disconnect();
+        console.log("DB disconnected");
+    }
+}
+main();
