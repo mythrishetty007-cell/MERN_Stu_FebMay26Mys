50982cac81fcb5127a889c11ea3210c07621f4df
mythrishetty007-cell <mythrishetty007@gmail.com>
2026-04-21 15:00:52 +0530

    Added auth rotes

--- Projects/Guided Project/BookMyShowClone/MERN Week 3_4_GuidProject_V1_ExpressMongo/backend/app.js
+++ Projects/Guided Project/BookMyShowClone/MERN Week 3_4_GuidProject_V1_ExpressMongo/backend/app.js
@@ -1,13 +1,17 @@
 const express = require("express");
+const authRoutes = require("./routes/auth.routes")
 
 const app = express();
 
 app.use(express.json());
+app.use("/api/auth",authRoutes);
 
+// Base URL
 app.get("/",(req,res)=>{
     res.status(200).json({
         success:true,
         message: "Movie booking API is running...",
     });
 });
+
 module.exports = app;
