4e68b0b9393bf77af6c37449595f9940d91f1bf9
mythrishetty007-cell <mythrishetty007@gmail.com>
2026-04-28 15:42:50 +0530

    Prop basics structure and style

--- W9/D2/prop-example-app/src/App.jsx
+++ W9/D2/prop-example-app/src/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from 'react'
 import {PropBasics} from './components/P1'
+import {PropDestrcturing} from './components/P2'
 import './App.css'
 function App() {
   return (
     <>
-      <PropBasics/>
+      {/* <PropBasics/> */}
+      <PropDestrcturing/>
     </>
   )
 }
