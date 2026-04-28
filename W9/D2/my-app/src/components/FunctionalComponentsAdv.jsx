16e25b5a3e7cec7c4a7c4cbf445aac3a74ba0cf5
mythrishetty007-cell <mythrishetty007@gmail.com>
2026-04-28 12:34:09 +0530

    Functional component with list rendering

---
+++ W9/D2/my-app/src/components/FunctionalComponentsAdv.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+// List rendering
+const users = [
+    {id:1, role:"Student"},
+    {id:2, role:"Trainer"},
+    {id:3, role:"Admin"},
+    {id:4, role:"Developer"}
+];
+
+export function FunctionComp(){
+    return(
+        <div>
+            <h2>Functional components</h2>
+            {/* List rendering */}
+            {users.map((user)=>(
+                <p key={user.id} className="pclass">
+                    {user.role}
+                </p>
+            ))}
+        </div>
+    );
+}
