cf38290d41ca5b319c6b465e9e8cec5692676238
mythrishetty007-cell <mythrishetty007@gmail.com>
2026-04-28 15:25:45 +0530

    Props basics

---
+++ W9/D2/prop-example-app/src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+import {PropBasics} from './components/P1'
+import './App.css'
+function App() {
+  return (
+    <>
+      <PropBasics/>
+    </>
+  )
+}
+export default App
