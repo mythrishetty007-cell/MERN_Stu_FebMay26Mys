7063e132cbfc71bf58124bb2e99abcc0e16ab385
mythrishetty007-cell <mythrishetty007@gmail.com>
2026-04-29 10:23:05 +0530

    Prop drilling

--- W9/D2/prop-example-app/src/App.jsx
+++ W9/D2/prop-example-app/src/App.jsx
@@ -2,13 +2,15 @@ import { useState } from 'react'
 import {PropBasics} from './components/P1'
 import {PropDestrcturing} from './components/P2'
 import {PropsChildren} from './components/P3'
+import {PropDrillingDemo} from './components/P4'
 import './App.css'
 function App() {
   return (
     <>
       {/* <PropBasics/> 
       <PropDestrcturing/> */}
-      <PropsChildren/>
+      {/* <PropsChildren/> */}
+    <PropDrillingDemo/>
     </>
   )
 }
