7240f5a75bfc4e63f6e5e8dfa84e4c5e4886e4e8
mythrishetty007-cell <mythrishetty007@gmail.com>
2026-04-29 10:15:29 +0530

    Wrapper component

--- W9/D2/prop-example-app/src/App.jsx
+++ W9/D2/prop-example-app/src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import {PropBasics} from './components/P1'
 import {PropDestrcturing} from './components/P2'
+import {PropsChildren} from './components/P3'
 import './App.css'
 function App() {
   return (
     <>
-      {/* <PropBasics/> */}
-      <PropDestrcturing/>
+      {/* <PropBasics/> 
+      <PropDestrcturing/> */}
+      <PropsChildren/>
     </>
   )
 }
