3744fb866acf766635cb7f2b9342a9da60dff953
mythrishetty007-cell <mythrishetty007@gmail.com>
2026-05-04 15:41:02 +0530

    Derived state and conditional rendering

--- W10/D1/state-hooks-app/src/App.jsx
+++ W10/D1/state-hooks-app/src/App.jsx
@@ -2,12 +2,14 @@ import './App.css'
 import { UseStateBasics } from "./components/P1"
 import {UpdatingObjectsAndArraysState} from "./components/P2"
 import {EventHandlingState} from "./components/P3"
+import {DerivedStateCondRender} from "./components/P4"
 function App() {
   return (
     <>
       {/* <UseStateBasics/> */}
       {/* <UpdatingObjectsAndArraysState/> */}
-      <EventHandlingState/>
+      {/* <EventHandlingState/> */}
+      <DerivedStateCondRender/>
     </>
   )
 }
