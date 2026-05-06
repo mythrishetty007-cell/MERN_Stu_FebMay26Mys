a93b917f43c2fb2fa9197bca33a34dda069d2e83
mythrishetty007-cell <mythrishetty007@gmail.com>
2026-05-06 09:45:58 +0530

    useEffect for side effects

--- W10/D1/state-hooks-app/src/App.jsx
+++ W10/D1/state-hooks-app/src/App.jsx
@@ -3,13 +3,15 @@ import { UseStateBasics } from "./components/P1"
 import {UpdatingObjectsAndArraysState} from "./components/P2"
 import {EventHandlingState} from "./components/P3"
 import {DerivedStateCondRender} from "./components/P4"
+import {UseEffectBasics} from "./components/P5"
 function App() {
   return (
     <>
       {/* <UseStateBasics/> */}
       {/* <UpdatingObjectsAndArraysState/> */}
       {/* <EventHandlingState/> */}
-      <DerivedStateCondRender/>
+      {/* <DerivedStateCondRender/> */}
+      <UseEffectBasics/>
     </>
   )
 }
