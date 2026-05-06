3509643c0b57b8a58ed7fea68dfa33ef85706180
mythrishetty007-cell <mythrishetty007@gmail.com>
2026-05-06 13:03:21 +0530

    useRef hook

--- W10/D2/my-app/src/App.jsx
+++ W10/D2/my-app/src/App.jsx
@@ -1,13 +1,13 @@
 import './App.css'
 import {UseContextIntro} from './components/P1';
 import {CustomHooksIntro} from './components/P2';
-import { UseRefIntro } from './components/P3';
+import { UserRefIntro } from './components/P3';
 function App() {
   return (
     <>
       {/* <UseContextIntro /> */}
       {/* <CustomHooksIntro/> */}
-      <UseRefIntro />
+      <UserRefIntro />
     </>
   )
 }
