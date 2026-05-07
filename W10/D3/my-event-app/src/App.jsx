1fb0055d90c8b279e8a57da9c805bd5b87f004dd
mythrishetty007-cell <mythrishetty007@gmail.com>
2026-05-07 13:49:40 +0530

    Event Delegation in React

--- W10/D3/my-event-app/src/App.jsx
+++ W10/D3/my-event-app/src/App.jsx
@@ -3,13 +3,15 @@ import { EventBasics } from './components/P1'
 import { EventObject } from './components/P2'
 import { PassingArguments } from './components/P3'
 import {UseCallbackEvents} from './components/P5'
+import {EventDelegation } from './components/P6'
 function App() {
   return (
     <>
       {/* <EventBasics /> */}
       {/* <EventObject /> */}
       {/* <PassingArguments /> */}
-      <UseCallbackEvents/>
+      {/* <UseCallbackEvents/> */}
+      <EventDelegation />
     </>
   )
 }
