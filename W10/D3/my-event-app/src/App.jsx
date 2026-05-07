93682af29d26afe6cae84393f6753f66331320ce
mythrishetty007-cell <mythrishetty007@gmail.com>
2026-05-07 11:41:30 +0530

    Passing arguments to Event Handlers

--- W10/D3/my-event-app/src/App.jsx
+++ W10/D3/my-event-app/src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import { EventBasics } from './components/P1'
 import { EventObject } from './components/P2'
+import { PassingArguments } from './components/P3'
 function App() {
   return (
     <>
       {/* <EventBasics /> */}
-      <EventObject />
+      {/* <EventObject /> */}
+      <PassingArguments />
     </>
   )
 }
