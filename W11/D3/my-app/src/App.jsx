1b013c27018d60303c25bbed37f27f30b92daf08
mythrishetty007-cell <mythrishetty007@gmail.com>
2026-05-13 14:47:42 +0530

    useEffect Lifecycle

---
+++ W11/D3/my-app/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import {ManagingApiState} from './components/P1';
+import {UseEffectLifecycle} from './components/P2';
+function App() {
+  
+  return (
+    <>
+     {/* <ManagingApiState/> */}
+     <UseEffectLifecycle/>
+    </>
+  )
+}
+export default App
