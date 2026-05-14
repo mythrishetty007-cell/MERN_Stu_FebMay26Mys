732723c9fcf7be91ebc8a19200c06e782a8a5e24
mythrishetty007-cell <mythrishetty007@gmail.com>
2026-05-14 09:46:58 +0530

    Crud operation using Axios

--- W11/D3/my-app/src/App.jsx
+++ W11/D3/my-app/src/App.jsx
@@ -1,12 +1,16 @@
 import './App.css'
 import {ManagingApiState} from './components/P1';
 import {UseEffectLifecycle} from './components/P2';
+import {AxiosLifecycle} from './components/P3';
+import {CRUDOperation} from './components/P4';
 function App() {
   
   return (
     <>
      {/* <ManagingApiState/> */}
-     <UseEffectLifecycle/>
+     {/* <UseEffectLifecycle/> */}
+     {/* <AxiosLifecycle/> */}
+     <CRUDOperation/>
     </>
   )
 }
